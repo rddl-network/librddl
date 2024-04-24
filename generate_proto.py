eda104aa033a36e87804cb1a9efae31d712a6ad8
Jürgen Eckel <juergen@riddleandcode.com>
2024-04-24 16:29:50 +0200

    added support to generate python bindings
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- generate_proto.py
+++ generate_proto.py
@@ -6,13 +6,14 @@ import os
 import sys
 import shutil
 import subprocess
+from argparse import ArgumentParser
 
 
 protoc = shutil.which("protoc")
 protoc = "./protoc-linux-amd64"
 
 
-def generate_proto(source):
+def generate_proto(source, isPython = False):
     """
     Call the Protocol Compiler to generate a _pb2.py from the given
     .proto file.  Does nothing if the output already exists and is newer than
@@ -38,6 +39,9 @@ def generate_proto(source):
             "--c_out=./libs/proto",
             source,
         ]
+        if isPython:
+            protoc_command[3] = "--python_out=./python/proto"
+        
         if subprocess.call(protoc_command) != 0:
             sys.exit(-1)
 
@@ -47,6 +51,15 @@ if __name__ == "__main__":
         sys.stderr.write("protoc is not installed!\n")
         sys.exit(-1)
 
+    parser = ArgumentParser()
+    parser.add_argument("--python", action="store_true", default=False, help="Enable Python mode")
+    args = parser.parse_args()
+
+    if args.python:
+        print("Python mode is enabled")
+    else:
+        print("Python mode is disabled")
+
     # Find all the protos
     proto_files = []
     for root, dirs, files in os.walk("proto"):
@@ -55,4 +68,4 @@ if __name__ == "__main__":
                 proto_files.append(os.path.join(root, file))
 
     for fn in proto_files:
-        generate_proto(fn)
+        generate_proto(fn, args.python)
